3574fb3473f07fe653fa09039a811b5ad45a7e54
Sera <dev.sera20@gmail.com>
2020-02-08 00:28:43 +0300

    Add Story and StoryBrain structures and functionalities for buttons.

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -2,9 +2,6 @@
 //  ViewController.swift
 //  Destini-iOS13
 //
-//  Created by Angela Yu on 08/08/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
 
 import UIKit
 
@@ -14,11 +11,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        let userChoice = sender.currentTitle!
+    
+        storyBrain.nextStory(userChoice)
+        
+        updateUI()
+    }
+    
+    func updateUI(){
+        
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1Title(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2Title(), for: .normal)
+    }
 }
 
